386bfc623a2d907f2c9ea9b86ea24b79b0d5543e
mediaios <iosmediadev@gmail.com>
2019-06-17 14:30:17 +0800

    build 1.0.8

--- PhoneNetSDK/PhoneNetSDK/PhoneNetSDKConst.h
+++ PhoneNetSDK/PhoneNetSDK/PhoneNetSDKConst.h
@@ -21,7 +21,7 @@
 
 /***********      Global define       ***********/
 #define      PhoneNotification       [NSNotificationCenter defaultCenter]
-#define      PhoneNetSDKVersion      @"1.0.4"
+#define      PhoneNetSDKVersion      @"1.0.8"
 
 /***********      Ping model       ***********/
 #define   KPingIcmpIdBeginNum     8000
