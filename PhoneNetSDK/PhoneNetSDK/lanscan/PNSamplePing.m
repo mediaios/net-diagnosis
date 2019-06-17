98785db3d60b27ae2030382cf088ff5133016eae
mediaios <iosmediadev@gmail.com>
2019-06-17 15:00:43 +0800

    fix bug: release to cocoapods error

--- PhoneNetSDK/PhoneNetSDK/lanscan/PNSamplePing.m
+++ PhoneNetSDK/PhoneNetSDK/lanscan/PNSamplePing.m
@@ -14,14 +14,13 @@
 
 @interface PNSamplePing()
 {
-    int socket_client;
-    struct sockaddr_in remote_addr;
+    int lan_scan_socket_client;
+    struct sockaddr_in lan_scan_remote_addr;
 }
-@property (nonatomic,assign) BOOL isPing;
 
 @property (nonatomic,assign) BOOL isStopPingThread;
-@property (nonatomic,strong) NSString *host;
-@property (nonatomic,assign) int pingPacketCount;
+@property (nonatomic,strong) NSString *scanIp;
+@property (nonatomic,assign) int sendPacketCount;
 @end
 
 @implementation PNSamplePing
@@ -38,10 +37,10 @@
 - (void)stopPing
 {
     self.isStopPingThread = YES;
-    shutdown(socket_client, SHUT_RDWR);
-    close(socket_client);
-    [self.delegate simplePing:self finished:self.host];
-    log4cplus_debug("PhoneNetSDK-LanScanner", "scan ip %s end...",[self.host UTF8String]);
+    shutdown(lan_scan_socket_client, SHUT_RDWR);
+    close(lan_scan_socket_client);
+    [self.delegate simplePing:self finished:self.scanIp];
+    log4cplus_debug("PhoneNetSDK-LanScanner", "scan ip %s end...",[self.scanIp UTF8String]);
 }
 
 - (BOOL)isPing
@@ -52,20 +51,20 @@
 - (BOOL)settingUHostSocketAddressWithIp:(NSString *)host
 {
     const char *hostaddr = [host UTF8String];
-    memset(&remote_addr, 0, sizeof(remote_addr));
-    remote_addr.sin_addr.s_addr = inet_addr(hostaddr);
+    memset(&lan_scan_remote_addr, 0, sizeof(lan_scan_remote_addr));
+    lan_scan_remote_addr.sin_addr.s_addr = inet_addr(hostaddr);
     struct timeval timeout;
     timeout.tv_sec = 0;
     timeout.tv_usec = 1000*200;
-    socket_client = socket(AF_INET,SOCK_DGRAM,IPPROTO_ICMP);
+    lan_scan_socket_client = socket(AF_INET,SOCK_DGRAM,IPPROTO_ICMP);
     int nZero=0;
-    setsockopt(socket_client,SOL_SOCKET,SO_SNDBUF,(char *)&nZero,sizeof(nZero));
-    int res = setsockopt(socket_client, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+    setsockopt(lan_scan_socket_client,SOL_SOCKET,SO_SNDBUF,(char *)&nZero,sizeof(nZero));
+    int res = setsockopt(lan_scan_socket_client, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
     if (res < 0) {
         log4cplus_warn("PhoneNetSimplePing", "ping %s , set timeout error..\n",[host UTF8String]);
         return YES;
     }
-    remote_addr.sin_family = AF_INET;
+    lan_scan_remote_addr.sin_family = AF_INET;
     
     return YES;
 }
@@ -74,17 +73,17 @@
 {
     log4cplus_debug("PhoneNetSDK-LanScanner", "scan ip %s begin...",[ip UTF8String]);
     if ([self settingUHostSocketAddressWithIp:ip]) {
-        self.host = ip;
+        self.scanIp = ip;
     }
     
-    if (self.host == NULL) {
+    if (self.scanIp == NULL) {
         self.isStopPingThread = YES;
         log4cplus_warn("PhoneNetSDK-LanScanner", "There is no valid ip...\n");
         return;
     }
     
     if (count > 0) {
-        _pingPacketCount = count;
+        _sendPacketCount = count;
     }
     [PNetQueue pnet_quick_ping_async:^{
         [self sendAndrecevPingPacket];
@@ -100,9 +99,9 @@
         }
         uint16_t identifier = (uint16_t)(KPingIcmpIdBeginNum + index);
         UICMPPacket *packet = [PhoneNetDiagnosisHelper constructPacketWithSeq:index andIdentifier:identifier];
-        ssize_t sent = sendto(socket_client, packet, sizeof(UICMPPacket), 0, (struct sockaddr *)&remote_addr, (socklen_t)sizeof(struct sockaddr));
+        ssize_t sent = sendto(lan_scan_socket_client, packet, sizeof(UICMPPacket), 0, (struct sockaddr *)&lan_scan_remote_addr, (socklen_t)sizeof(struct sockaddr));
         if (sent < 0) {
-            log4cplus_warn("PhoneNetSDK-LanScanner", "ping %s , error code:%d, send icmp packet error..\n",[self.host UTF8String],(int)sent);
+            log4cplus_warn("PhoneNetSDK-LanScanner", "ping %s , error code:%d, send icmp packet error..\n",[self.scanIp UTF8String],(int)sent);
             [self stopPing];
             break;
         }
@@ -112,17 +111,17 @@
         socklen_t addrLen = sizeof(ret_addr);
         void *buffer = malloc(65535);
         
-        size_t bytesRead = recvfrom(socket_client, buffer, 65535, 0, (struct sockaddr *)&ret_addr, &addrLen);
+        size_t bytesRead = recvfrom(lan_scan_socket_client, buffer, 65535, 0, (struct sockaddr *)&ret_addr, &addrLen);
         
         if ((int)bytesRead < 0) {
-            [self.delegate simplePing:self didTimeOut:self.host];
+            [self.delegate simplePing:self didTimeOut:self.scanIp];
             res = YES;
         }else if(bytesRead == 0){
-            log4cplus_warn("PhoneNetSDK-LanScanner", "ping %s , receive icmp packet error , bytesRead=0",[self.host UTF8String]);
+            log4cplus_warn("PhoneNetSDK-LanScanner", "ping %s , receive icmp packet error , bytesRead=0",[self.scanIp UTF8String]);
         }else{
             
             if ([PhoneNetDiagnosisHelper isValidPingResponseWithBuffer:(char *)buffer len:(int)bytesRead]) {
-                [self.delegate simplePing:self receivedPacket:self.host];
+                [self.delegate simplePing:self receivedPacket:self.scanIp];
                 [self stopPing];
                 break;
             }
@@ -132,9 +131,9 @@
             index++;
         }
         usleep(1000);
-    } while (!self.isStopPingThread && index < _pingPacketCount);
+    } while (!self.isStopPingThread && index < _sendPacketCount);
     
-    if (index == _pingPacketCount) {
+    if (index == _sendPacketCount) {
         [self stopPing];
     }
     
